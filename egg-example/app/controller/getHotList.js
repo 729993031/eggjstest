c7ea83819241eb1eeacc96211d3ec064e3111ef2
Administrator <729993031@qq.com>
2018-08-24 21:37:15 +0800

    second

--- egg-example/app/controller/getHotList.js
+++ egg-example/app/controller/getHotList.js
@@ -2,22 +2,21 @@ const Controller = require('egg').Controller;
 
 class getHotList extends Controller{
     async index(){
-        const ctx = this.ctx;
-        const ctxQuery = ctx.query;
-        const result = await  ctx.curl('http://m.you.163.com/xhr/topic/getHotList.json', {
+        const {ctx, service} = this;
+        /*ctx.header = {'Access-Control-Allow-Origin':' *'};*/
+        return ctx.body = await service.requestCache.judge('http://m.you.163.com/xhr/topic/getHotList.json', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
             },
             data: {
-                page:ctxQuery.page,
-                size:ctxQuery.size
+                page: ctx.query.page,
+                size: ctx.query.size
             },
             dataType: 'json',
             timeout: [ 3000, 10000 ],
         });
         /*ctx.header = {'Access-Control-Allow-Origin':' *'};*/
-        ctx.body = result.data;
     }
 }
 
