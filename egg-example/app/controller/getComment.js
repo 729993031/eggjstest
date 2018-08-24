47d2918630ed0827c78a024af5f05549706fd255
Administrator <729993031@qq.com>
2018-08-24 22:16:57 +0800

    second

--- egg-example/app/controller/getComment.js
+++ egg-example/app/controller/getComment.js
@@ -2,23 +2,11 @@ const Controller = require('egg').Controller;
 
 class getComment extends Controller{
     async index(){
-        const ctx = this.ctx;
-        const ctxQuery = ctx.query;
-        const result = await  ctx.curl('http://m.you.163.com/xhr/comment/listByItemByTag.json', {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
-            },
-            data: {
-                tag:ctxQuery.tag,
-                itemId:ctxQuery.itemId,
-                page:ctxQuery.page
-            },
-            dataType: 'json',
-            timeout: [ 3000, 10000 ],
-        });
+        const {ctx, service} = this;
+        const {tag,itemId,page} = ctx.query;
         /*ctx.header = {'Access-Control-Allow-Origin':' *'};*/
-        ctx.body = result.data;
+        return ctx.body = await  service.requestCache.judge(`
+        http://m.you.163.com/xhr/comment/listByItemByTag.json?tag=${tag}itemId=${itemId}page=${page}`});
     }
 }
 
